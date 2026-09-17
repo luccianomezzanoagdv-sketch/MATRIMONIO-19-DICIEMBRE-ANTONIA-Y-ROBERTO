9bae9b47f7e68ece86e9fd5e04ce999d8b8dd236
luccianomezzanoagdv-sketch <luccianomezzanoagdv@gmail.com>
2026-09-17 13:58:29 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
    No reemplaces el config.js de un matrimonio existente por esta copia vacía.
    Usa CLIENT_TOKEN. Nunca escribas ADMIN_TOKEN en este archivo. */
 window.AGDV_PUBLIC_SYNC = Object.freeze({
-  url: '',
-  clientToken: ''
+  url: 'https://script.google.com/macros/s/AKfycbwRaRwbPDY76wobEdngE8xQ1kSLRGtn5oK1o3Ft5CeEH9L35F0Gc9YOK4mtglH1Lq_6/exec',
+  clientToken: '111213141516171819'
 });
